225ef719e22f3fe5a5bf5cee7d65ffd84524a59a
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-08-01 09:44:56 -0600

    initial commit.

---
+++ MathematicalSets.py
@@ -0,0 +1,14 @@
+#/**
+ #* *Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+    # *@author Sara Echeverria
+    # *FileName: 
+    # @version: I
+    #- Creation: 01/08/2022
+    #- Last modification: 01/08/2022
+
+# Definition
+def a( ):
+    print(header[0], end = "\t\t\t")
+for i in range(1, 7):
+    print(header[i], end = "\t")
+    print()
